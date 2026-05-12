0ae0463329696da9533bf0e0bbae00f18ac11c45
JulesPicard <jlspcrd@gmail.com>
2026-05-12 16:24:06 +0200

    Update start.c

--- start.c
+++ start.c
@@ -1,6 +1,6 @@
 #include "Projet.h"
 
-void start(Joueur joueurs, int* nb_joueurs){
+void start(Joueur *joueurs, int* nb_joueurs){
     printf("Combien de joueurs ?");
     scanf("%d",nb_joueurs);
     for(int i=0;i<*nb_joueurs;i++){
