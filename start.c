9914acc98f524724e69941dba8a99a9aad367e00
JulesPicard <jlspcrd@gmail.com>
2026-05-12 16:04:12 +0200

    Add files via upload

--- start.c
+++ start.c
@@ -4,7 +4,10 @@ void start(Joueur joueurs, int* nb_joueurs){
     printf("Combien de joueurs ?");
     scanf("%d",nb_joueurs);
     for(int i=0;i<*nb_joueurs;i++){
-        printf("Entrez votre pseudo");
-        scanf("%s",joueurs);
+        printf("Entrez le nom du joueur %d", i+1);
+        scanf("%s",joueurs[i].nom);
+
+        joueurs[i].score=0;
+        joueurs[i].actif=1;
     }
 }
