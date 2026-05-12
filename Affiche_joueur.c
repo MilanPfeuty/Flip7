cd11593cd58ba04f3c38a6b561f37c54f62ac1c3
JulesPicard <jlspcrd@gmail.com>
2026-05-12 15:28:54 +0200

    Update Affiche_joueur.c

--- Affiche_joueur.c
+++ Affiche_joueur.c
@@ -1,11 +1,11 @@
 #include 'Projet.h'
 
-void afficher_joueur(int score, int cartes[], int taille) {
+void afficher_joueur(int score, Cartes cartes[], int taille) {
     printf("\n--- JOUEUR ---\n");
 
     printf("Main : ");
     for (int i = 0; i < taille; i++) {
-        printf("%d ", cartes[i]);
+        printf("%d ", cartes[i].numero);
     }
 
     printf("\nScore : %d\n", score);
