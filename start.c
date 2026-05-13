83f054e40fc1b4c1422621d594cba1ae179e56ac
MilanPfeuty <mpfeuty@gmail.com>
2026-05-13 16:41:21 +0200

    Add files via upload

--- start.c
+++ start.c
@@ -4,10 +4,10 @@ int start(){
     int nb_joueurs[10];
     Joueur *joueurs;
 
-    printf("Combien de joueurs ?");
+    printf("Combien de joueurs ? : ");
     scanf("%d",nb_joueurs);
     for(int i = 0 ;i < *nb_joueurs ; i++){
-        printf("Entrez le nom/pseudo du joueur %d", i+1);
+        printf("Entrez le nom/pseudo du joueur %d : ", i+1);
         scanf("%s",joueurs[i].nom);
 
         joueurs[i].score=0;
