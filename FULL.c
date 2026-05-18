5133648a76e41ce0fa3b8f950a0c6528e14086d0
MilanPfeuty <mpfeuty@gmail.com>
2026-05-18 09:58:52 +0200

    Update FULL.c

--- FULL.c
+++ FULL.c
@@ -149,17 +149,34 @@ void restitution_score(Joueur* j){
 
 
 
-int start(Joueur joueurs[], int *nb_joueurs){
+void vide_buffer() {
+    while (getchar() != '\n');
+}
 
-    for(int i = 0 ;i < *nb_joueurs ; i++){
-        printf("Entrez le nom/pseudo du joueur %d : ", i+1);
-        scanf("%s",joueurs[i].nom);
+int start(Joueur joueurs[], int *nb_joueurs) {
+
+    int verif = 0;
+
+    do {
+        printf("Combien de joueurs ? : ");
+        verif = scanf("%d", nb_joueurs);
+        vide_buffer();
+
+        if (verif != 1) {
+            printf("Erreur, entrer un entier\n");
+        }
+    } while (verif != 1);
+
+    for (int i = 0; i < *nb_joueurs; i++) {
+        printf("Nom du joueur %d : ", i + 1);
+        scanf("%s", joueurs[i].nom);
 
         joueurs[i].score = 0;
         joueurs[i].actif = 1;
         joueurs[i].nb_carte = 0;
     }
-    return 0;
+
+    return 1;
 }
 
 
