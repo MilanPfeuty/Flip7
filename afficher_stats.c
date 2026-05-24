b9570b0d4713e78f9529415acfa737810141d1f5
Damien <damien.rouge95@gmail.com>
2026-05-24 23:02:27 +0200

    Refactor afficher_stats to show only drawn cards
    Updated the afficher_stats function to display only drawn cards and bonuses that have been drawn.

--- afficher_stats.c
+++ afficher_stats.c
@@ -2,19 +2,43 @@
 
 void afficher_stats(int stats_numero[], int stats_bonus[]) {
 
-    printf("\n=== STATISTIQUES DES CARTES ===\n");
+    printf("\n=== CARTES DEJA PIOCHEES ===\n");
 
+    // Affiche uniquement les cartes normales déjà été picohés
     for (int i = 0; i < 13; i++) {
 
-        printf("Carte %d : %d piochée(s)\n",
-               i,
-               stats_numero[i]);
+        if (stats_numero[i] > 0) {
+
+            printf(
+                "Carte %d : %d piochée(s)\n",
+                i,
+                stats_numero[i]
+            );
+        }
     }
 
-    printf("+2 : %d\n", stats_bonus[0]);
-    printf("+4 : %d\n", stats_bonus[1]);
-    printf("+6 : %d\n", stats_bonus[2]);
-    printf("+8 : %d\n", stats_bonus[3]);
-    printf("+10 : %d\n", stats_bonus[4]);
-    printf("x2 : %d\n", stats_bonus[5]);
+    // Affiche les bonus seulement s'ils sont déjà été piochés
+    if (stats_bonus[0] > 0) {
+        printf("+2 : %d\n", stats_bonus[0]);
+    }
+
+    if (stats_bonus[1] > 0) {
+        printf("+4 : %d\n", stats_bonus[1]);
+    }
+
+    if (stats_bonus[2] > 0) {
+        printf("+6 : %d\n", stats_bonus[2]);
+    }
+
+    if (stats_bonus[3] > 0) {
+        printf("+8 : %d\n", stats_bonus[3]);
+    }
+
+    if (stats_bonus[4] > 0) {
+        printf("+10 : %d\n", stats_bonus[4]);
+    }
+
+    if (stats_bonus[5] > 0) {
+        printf("x2 : %d\n", stats_bonus[5]);
+    }
 }
